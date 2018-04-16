14d8de5ab6ee62158f87607cf75a5b71f04ba8e6
kim hanwoong <d10191@idea.co.kr>
2018-04-16 17:21:47 +0900

    엑셀양식으로 바뀌더라도 문제가 없도록 시간변환 포멧을 추가함.

--- ditime.py
+++ ditime.py
@@ -42,5 +42,19 @@ def ToFullTime(t):
 		return '%sT19:00:00+09:00' % (t), None
 	return t, "약속한 시간포멧 형태가 아닙니다."
 
+def ToExcelTime(t):
+	"""
+	엑셀에 날짜를 넣을 때 문자가 없다면, 엑셀이 숫자로 인식하며,
+	0725 날짜가 725로 바뀐다. 중간에 "/" 문자를 넣을 필요가 있다.
+	2017-07-25T16:21:47+09:00 또는 2017-07-25, 0725 형태의 날짜를 07/25로 바꾸어준다.
+	"""
+	if re.search('\d{4}$',t):
+		return t[0:2]+"/"+t[2:], None
+	if re.search('\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[-+]\d{2}:\d{2}$', t):
+		return t[5:10].replace("-","/"), None
+	if re.search('\d{4}-\d{2}-\d{2}', t):
+		return t[5:7]+"/"+t[8:10], None
+	return t, "약속한 시간포멧 형태가 아닙니다."
+
 if __name__ == "__main__":
 	print ToNormalTime("2017-08-08T23:45:23+09:00")
