151b4d87b70462e982a46ee53c7263dfee80472c
kim han woong <khw7096@gmail.com>
2019-11-19 19:04:23 +0900

    YYYYMMDD, MMDD 패턴추가함.

--- ditime.go
+++ ditime.go
@@ -8,9 +8,13 @@ import (
 )
 
 var regexpShortTime = regexp.MustCompile(`^\d{4}$`)                                                                            // 1019
-var regexpExcelTime = regexp.MustCompile(`^\d{2}/\d{2}$`)                                                                      // 10/19
 var regexpNormalTime = regexp.MustCompile(`^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$`)                                // 2016-10-19
 var regexpFullTime = regexp.MustCompile(`^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])T\d{2}:\d{2}:\d{2}[-+]\d{2}:\d{2}$`) // 2016-10-19T16:41:24+09:00
+
+// RegexpMMDD 레귤러 익스프레션은 MM*DD 패턴의 날짜를 처리한다.
+var RegexpMMDD = regexp.MustCompile(`^(0?[1-9]|1[012])[-.,/]\s?(0?[1-9]|[12][0-9]|3[01])[,.]*$`) // 10/19, 10-19, 10.10. 1.1 1.1. "1. 1."
+// RegexpYYYYMMDD 레귤러 익스프레션은 YYYY*MM*DD* 패턴의 날짜를 처리한다.
+var RegexpYYYYMMDD = regexp.MustCompile(`^\d{4}[-,./]\s?(0?[1-9]|1[012])[-,./]\s?(0?[1-9]|[12][0-9]|3[01])[,.]*$`) // 2016-10-19, 2016/10/19, 2016.10.19, 2016.10.19. 2016. 10. 10.
 
 // Now 함수는 디지털아이디어에서 사용하는 서비스의 현재 시간을 RFC3339 포멧으로 반환한다.
 func Now() string {
@@ -65,7 +69,7 @@ func ToFullTime(mode, t string) (string, error) {
 		sec = currentTime.Second()
 		nsec = currentTime.Nanosecond()
 	default:
-		return t, errors.New("mode에는 start,end,current를 입력할 수 있습니다")
+		return t, errors.New("mode에는 start,end,current 값만 입력할 수 있습니다")
 	}
 	if regexpShortTime.MatchString(t) {
 		m, err := strconv.Atoi(t[0:2])
@@ -78,7 +82,7 @@ func ToFullTime(mode, t string) (string, error) {
 		}
 		t := time.Date(time.Now().Year(), time.Month(m), d, hour, min, sec, nsec, time.Local)
 		return t.Format(time.RFC3339), nil
-	} else if regexpExcelTime.MatchString(t) {
+	} else if RegexpMMDD.MatchString(t) {
 		m, err := strconv.Atoi(t[0:2])
 		if err != nil {
 			return t, err
@@ -128,6 +132,42 @@ func ToFullTime(mode, t string) (string, error) {
 			}
 			return t, nil
 		}
+	} else if RegexpYYYYMMDD.MatchString(t) {
+		re, err := regexp.Compile(`^(\d+).(\d+).(\d+)`)
+		if err != nil {
+			return t, err
+		}
+		result := re.FindStringSubmatch(t)
+		y, err := strconv.Atoi(result[1])
+		if err != nil {
+			return t, err
+		}
+		m, err := strconv.Atoi(result[2])
+		if err != nil {
+			return t, err
+		}
+		d, err := strconv.Atoi(result[3])
+		if err != nil {
+			return t, err
+		}
+		t := time.Date(y, time.Month(m), d, hour, min, sec, nsec, time.Local)
+		return t.Format(time.RFC3339), nil
+	} else if RegexpMMDD.MatchString(t) {
+		re, err := regexp.Compile(`^(\d+)[.,-/]\s?(\d+)[.,]*$`)
+		if err != nil {
+			return t, err
+		}
+		result := re.FindStringSubmatch(t)
+		m, err := strconv.Atoi(result[1])
+		if err != nil {
+			return t, err
+		}
+		d, err := strconv.Atoi(result[2])
+		if err != nil {
+			return t, err
+		}
+		t := time.Date(time.Now().Year(), time.Month(m), d, hour, min, sec, nsec, time.Local)
+		return t.Format(time.RFC3339), nil
 	} else {
 		return t, errors.New(`입력한 날짜형식이 "0113","1982-01-13","1982-01-13T10:38:37+09:00" 형태가 아닙니다`)
 	}
--- ditime_test.go
+++ ditime_test.go
@@ -8,10 +8,10 @@ import (
 	"github.com/digital-idea/ditime"
 )
 
-func Test_ToFullTime(t *testing.T) {
+func TestToFullTime(t *testing.T) {
 	n := time.Now()
 	// travisCI 에서는 UTC형식인 "2018-06-18T19:00:00Z" 라고 RFC3339 형식의 시간이 표기된다.
-	timeOffset := n.Format(time.RFC3339)[19:len(n.Format(time.RFC3339))]
+	timeZone := n.Format(time.RFC3339)[19:len(n.Format(time.RFC3339))]
 	cases := []struct {
 		in   string
 		mode string
@@ -20,22 +20,32 @@ func Test_ToFullTime(t *testing.T) {
 	}{{
 		in:   "0618",
 		mode: "start",
-		want: fmt.Sprintf("%04d-06-18T10:00:00%s", n.Year(), timeOffset),
+		want: fmt.Sprintf("%04d-06-18T10:00:00%s", n.Year(), timeZone),
 		err:  nil,
 	}, {
 		in:   "0618",
 		mode: "end",
-		want: fmt.Sprintf("%04d-06-18T19:00:00%s", n.Year(), timeOffset),
+		want: fmt.Sprintf("%04d-06-18T19:00:00%s", n.Year(), timeZone),
+		err:  nil,
+	}, {
+		in:   "06.18",
+		mode: "end",
+		want: fmt.Sprintf("%04d-06-18T19:00:00%s", n.Year(), timeZone),
+		err:  nil,
+	}, {
+		in:   "06.19.",
+		mode: "end",
+		want: fmt.Sprintf("%04d-06-18T19:00:00%s", n.Year(), timeZone),
 		err:  nil,
 	}, {
 		in:   "06/18",
 		mode: "start",
-		want: fmt.Sprintf("%04d-06-18T10:00:00%s", n.Year(), timeOffset),
+		want: fmt.Sprintf("%04d-06-18T10:00:00%s", n.Year(), timeZone),
 		err:  nil,
 	}, {
 		in:   "2018-06-18",
 		mode: "start",
-		want: fmt.Sprintf("2018-06-18T10:00:00%s", timeOffset),
+		want: fmt.Sprintf("2018-06-18T10:00:00%s", timeZone),
 		err:  nil,
 	}, {
 		in:   "2018-06-18T18:45:23+09:00",
@@ -45,18 +55,124 @@ func Test_ToFullTime(t *testing.T) {
 	}, {
 		in:   "2018-06-18T18:45:23+09:00",
 		mode: "start",
-		want: fmt.Sprintf("2018-06-18T10:00:00%s", timeOffset),
+		want: fmt.Sprintf("2018-06-18T10:00:00%s", timeZone),
 		err:  nil,
 	}, {
 		in:   "2018-06-18T18:45:23+09:00",
 		mode: "end",
-		want: fmt.Sprintf("2018-06-18T19:00:00%s", timeOffset),
+		want: fmt.Sprintf("2018-06-18T19:00:00%s", timeZone),
 		err:  nil,
 	}}
 	for _, c := range cases {
 		result, err := ditime.ToFullTime(c.mode, c.in)
 		if result != c.want {
 			t.Fatalf("Test_ToFullTime(%v,%v): 얻은 값 %v, 원하는 값 %v, 에러 %v", c.mode, c.in, result, c.want, err)
+		}
+	}
+}
+
+func TestRegexYYYYMMDD(t *testing.T) {
+	cases := []struct {
+		time string
+		want bool
+	}{{
+		time: "2019-11-19",
+		want: true,
+	}, {
+		time: "2019/11/19",
+		want: true,
+	}, {
+		time: "2019/1/13",
+		want: true,
+	}, {
+		time: "2019/1/1",
+		want: true,
+	}, {
+		time: "2019.11.19.",
+		want: true,
+	}, {
+		time: "2019,11,19",
+		want: true,
+	}, {
+		time: "2019,11,19,",
+		want: true,
+	}, {
+		time: "2019-1-19",
+		want: true,
+	}, {
+		time: "2019-1-1",
+		want: true,
+	}, {
+		time: "2019. 1. 1.",
+		want: true,
+	}, {
+		time: "2019, 1, 1,",
+		want: true,
+	}, {
+		time: "2019, 1, 1",
+		want: true,
+	}, {
+		time: "2019. 1. 1",
+		want: true,
+	},
+	}
+	for _, c := range cases {
+		reg := ditime.RegexpYYYYMMDD
+		if reg.MatchString(c.time) != c.want {
+			t.Fatalf("Test_regexYYYYMMDD: 입력 값 %v, 원하는 값 %v, 결과 %v", c.time, c.want, reg.MatchString(c.time))
+		}
+	}
+}
+
+func TestRegexMMDD(t *testing.T) {
+	cases := []struct {
+		time string
+		want bool
+	}{{
+		time: "11-19",
+		want: true,
+	}, {
+		time: "11/19",
+		want: true,
+	}, {
+		time: "1/13",
+		want: true,
+	}, {
+		time: "1/1",
+		want: true,
+	}, {
+		time: "11.19.",
+		want: true,
+	}, {
+		time: "11,19",
+		want: true,
+	}, {
+		time: "11,19,",
+		want: true,
+	}, {
+		time: "1-19",
+		want: true,
+	}, {
+		time: "1-1",
+		want: true,
+	}, {
+		time: "1. 1.",
+		want: true,
+	}, {
+		time: "1, 1,",
+		want: true,
+	}, {
+		time: "1, 1",
+		want: true,
+	}, {
+		time: "1. 1",
+		want: true,
+	},
+	}
+	for _, c := range cases {
+		reg := ditime.RegexpMMDD
+		if reg.MatchString(c.time) != c.want {
+			t.Fatalf("Test_regexMMDD: 입력 값 %v, 원하는 값 %v, 결과 %v", c.time, c.want, reg.MatchString(c.time))
 		}
 	}
 }
